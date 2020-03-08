c625b367a413ae0f14d4e16abdf8f1a3aaf7aeee
Vitor <Vitor@Vitor-PC>
2020-03-08 17:36:13 -0300

    exercício de treino alterado após a aula de dia 4

--- src/main/java/model/dao/BaseDAO.java
+++ src/main/java/model/dao/BaseDAO.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 
 /**
  * 
- * Interface que contÈm os mÈtodos que todos os DAOs devem implementar.
+ * Interface que cont√©m os m√©todos que todos os DAOs devem implementar.
  * 
- * T È um tipo genÈrico, que indica a classe que implementar· o DAO.
+ * T √© um tipo gen√©rico, que indica a classe que implementar√° o DAO.
  * 
  * Ex.: para a entidade Pessoa, deve ser criada uma classe PessoaDAO que
  * implementa BaseDAO<Pessoa>.
  * 
- * @author Vilmar CÈsar Pereira J˙nior
- * @param <T> o tipo da classe de entidade (ou VO) que o DAO implementar·
+ * @author Vilmar C√©sar Pereira J√∫nior
+ * @param <T> o tipo da classe de entidade (ou VO) que o DAO implementar√°
  * 
  */
 public interface BaseDAO<T> {
@@ -21,7 +21,7 @@ public interface BaseDAO<T> {
 	/**
 	 * Insere um novo registro na tabela de entidade T
 	 * 
-	 * @param novaEntidade o objeto que contÈm o novo registro que ser· inserido na
+	 * @param novaEntidade o objeto que cont√©m o novo registro que ser√° inserido na
 	 *                     tabela.
 	 * 
 	 * @return a novaEntidade salva, agora contendo um id.
@@ -29,10 +29,10 @@ public interface BaseDAO<T> {
 	public T salvar(T novaEntidade);
 
 	/**
-	 * Exclui um determinado registro na tabela T, dado a sua chave prim·ria.
+	 * Exclui um determinado registro na tabela T, dado a sua chave prim√°ria.
 	 * 
-	 * @param id a chave prim·ria da entidade.
-	 * @return true caso excluiu, false caso contr·rio.
+	 * @param id a chave prim√°ria da entidade.
+	 * @return true caso excluiu, false caso contr√°rio.
 	 */
 	public boolean excluir(int id);
 
@@ -40,16 +40,16 @@ public interface BaseDAO<T> {
 	 * Altera um determinado registro na tabela T, dado o objeto escolhido para ser
 	 * alterado.
 	 * 
-	 * @param entidade o objeto que ter· o registro atualizado na tabela T.
-	 * @return true caso atualizou, false caso contr·rio.
+	 * @param entidade o objeto que ter√° o registro atualizado na tabela T.
+	 * @return true caso atualizou, false caso contr√°rio.
 	 */
 	public boolean alterar(T entidade);
 
 	/**
-	 * Retorna um objeto do tipo T, dado a sua chave prim·ria.
+	 * Retorna um objeto do tipo T, dado a sua chave prim√°ria.
 	 * 
-	 * @param id a chave prim·ria do objeto/registro buscado
-	 * @return o objeto retornado pela consulta, ou null caso n„o exista registro com o id informado.
+	 * @param id a chave prim√°ria do objeto/registro buscado
+	 * @return o objeto retornado pela consulta, ou null caso n√£o exista registro com o id informado.
 	 */
 	public T consultarPorId(int id);
 
